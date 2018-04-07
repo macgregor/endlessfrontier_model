91ed200a26be2c605274da7e037a890542b8bcb6
Matthew Stratton <mstratto@redhat.com>
2018-04-07 18:33:07 -0400

    changing the damn password to not have special characters

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_447b533b539c_key -iv $encrypted_447b533b539c_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_1d6fee8c9472_key -iv $encrypted_1d6fee8c9472_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 fi
